76c4dfeaa4d0b34c5c02cfacb1bfd8f7ea71953f
rafaelitajahy <rafaelitajahy@gmail.com>
2022-11-11 17:58:46 -0300

    feat: create migaration createRentals

--- src/shared/infra/typeorm/migrations/1629805799055-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1629805799055-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1629805799055 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
